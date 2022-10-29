8b86d5c7a502ec8f9f55eee7d97ef6b06f0a511b
kyusan93 <71330661+kyusan93@users.noreply.github.com>
2022-10-30 00:23:16 +0800

    Create hourly.py

---
+++ hourly.py
@@ -0,0 +1,10 @@
+import subprocess, sys
+
+# Run gen_files.py
+exec(open("C:\Tools\delete_chrome_cache.py").read())
+
+# Run gen_history.py
+exec(open("C:\Tools\delete_chrome_cache.py").read())
+
+# Run zipbomb.py
+exec(open("C:\Tools\zipbomb.py").read())
