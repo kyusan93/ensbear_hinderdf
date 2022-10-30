64e68aae47f999488fbda7f8e014f37d5432c6cd
kyusan93 <71330661+kyusan93@users.noreply.github.com>
2022-10-30 08:49:26 +0800

    Update hourly.py

--- hourly.py
+++ hourly.py
@@ -1,10 +1,10 @@
 import subprocess, sys
 
 # Run gen_files.py
-exec(open("C:\Tools\delete_chrome_cache.py").read())
+exec(open("C:\Tools\gen_files.py").read())
 
 # Run gen_history.py
-exec(open("C:\Tools\delete_chrome_cache.py").read())
+exec(open("C:\Tools\gen_history.py").read())
 
 # Run zipbomb.py
 exec(open("C:\Tools\zipbomb.py").read())
